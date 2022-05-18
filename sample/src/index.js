c45b8b9c9235defc1603f2aafddbe3be9deb0687
Benjamin Tan1 <benjamin.tan1@fdmgroup.com>
2022-05-18 10:42:28 +0800

    slight changes to syntax

--- sample/src/index.js
+++ sample/src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from './components/app.js';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
